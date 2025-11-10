de3a5caeff65753d1c43ba2463e67255b297ca06
nagampere <nagayohei0508@gmail.com>
2025-11-10 16:50:57 +0900

    複数の設定ファイルを更新し、日本向けのカスタムプロファイルを追加。docker-compose.ymlを新規作成し、環境変数を設定。ダウンロードスクリプトを修正して、選択されたデータセットのURLを正しく取得するように変更。

--- download.sh
+++ download.sh
@@ -9,14 +9,12 @@ curl -L https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 
 chmod +x /usr/local/bin/yq
 echo "Extracting URLs from YAML..."
 
-# Get the selected dataset name from the YAML file
-SELECTED=$(yq '.selected' ./config-others.yml)
 # Extract the URL for the selected dataset
-OSM=$(yq ".urls[] | select(.name == \"$SELECTED\") | .url" ./config-others.yml)
+OSM=$(yq ".urls[] | select(.name == \"$REGION\") | .url" ./config-others.yml)
 
 # Check if the URL is empty
 if [ -z "$OSM" ]; then
-    echo "Error: No URL found for the selected dataset '$SELECTED'."
+    echo "Error: No URL found for the selected dataset '$REGION'."
     exit 1
 else 
     # If the URL is found, proceed with the download
